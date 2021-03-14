ccda3c5e388de39b036084b0e475c783795c56bb
FoeverNa <foevna@naver.com>
2021-03-14 11:14:10 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/demospringbootwebservice/DemoSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demospringbootwebservice/DemoSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
