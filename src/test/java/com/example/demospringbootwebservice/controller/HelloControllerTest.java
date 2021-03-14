fa18cb08a706d5a17426d4a352eb9f14aabb22c7
FoeverNa <foevna@naver.com>
2021-03-14 12:58:09 +0900

    helloDto 메소드 추가 및 테스트

--- src/test/java/com/example/demospringbootwebservice/controller/HelloControllerTest.java
+++ src/test/java/com/example/demospringbootwebservice/controller/HelloControllerTest.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers  = HelloController.class)
 class HelloControllerTest {
@@ -22,6 +22,22 @@ class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+//                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.amount").value(amount))
+                .andDo(print());
     }
 
 }
