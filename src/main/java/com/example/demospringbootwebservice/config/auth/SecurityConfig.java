823e8971a56436bcc5215412bb208d0aa1647073
FoeverNa <foevna@naver.com>
2021-04-07 14:53:34 +0900

    Securityconfig 주석작성

--- src/main/java/com/example/demospringbootwebservice/config/auth/SecurityConfig.java
+++ src/main/java/com/example/demospringbootwebservice/config/auth/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity // SpringSecurity 설정 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOauth2UserService customOauth2UserService;
@@ -16,19 +16,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .headers().frameOptions().disable()
+                .headers().frameOptions().disable()//h2-console 화면 사용하기 위해 disable
                 .and()
-                    .authorizeRequests()
+                    .authorizeRequests()//URL별 권한 관리 설정하는 옵션의 시작점
                     .antMatchers("/", "/css/**", "/images/**",
                             "/js/**", "/h2-console/**").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //USER 권한만 열람하도록함
+                    .anyRequest().authenticated()// 설정된 값들 이외 나머지 URL들은 인증된 사용자에게만 제공
                 .and()
-                    .logout()
+                    .logout()// logout 설정의 진입점
                         .logoutSuccessUrl("/")
                 .and()
-                    .oauth2Login()
-                        .userInfoEndpoint()
-                            .userService(customOauth2UserService);
+                    .oauth2Login()// OAuth2 로그인 기능에 대한 설정 진입점
+                        .userInfoEndpoint()//Oauth2 로그인 성공 이후 사용자 정보를 가져올대의 설정담당
+                            .userService(customOauth2UserService);//
     }
 }
